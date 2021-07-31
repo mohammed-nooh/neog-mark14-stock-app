cb203a0b459aaf20f93f65a43ed1609c3d300bca
mohammed-nooh <83730829+mohammed-nooh@users.noreply.github.com>
2021-07-31 13:22:12 +0530

    feat : added animation for profit and loss

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 
 
 const inputs=document.querySelectorAll(".input");
-
+const body=document.querySelector("body");
 const subBtn=document.querySelector("#subBtn");
 const message=document.querySelector(".message");
+const heading=document.querySelector(".heading");
 
 function showMessage(text){
     message.classList.remove("hidden");
@@ -13,26 +14,41 @@ function showMessage(text){
 
 
 function check(){
+    heading.style.color="black";
+    body.style.background="none";
     message.classList.add("hidden");
 
     const cp=Number(inputs[0].value);
     const quantity=Number(inputs[1].value);
     const sp=Number(inputs[2].value);
-    console.log(cp,quantity,sp);
-    console.log(cp-sp);
+    // console.log(cp,quantity,sp);
     if(cp>0 && quantity>0 && sp>0){
         //loss
         if(cp>sp){
             const loss=((cp-sp)*quantity).toFixed(2);
             const lossPer=((cp-sp)*100/cp).toFixed(2);
             showMessage(`You lost ${lossPer}%. Your total loss is ${loss}`);
-            console.log(`You lost ${lossPer}%. Your total loss is  ${loss}`);
+
+            if(lossPer>50){
+                heading.style.color="white";
+                body.style.background="url(gifs/sad.gif)";
+            }
+            
 
         }
         //profit
         else{
-            
+            const profit=((sp-cp)*quantity).toFixed(2);
+            const profitPer=((sp-cp)*100/cp).toFixed(2);
+            showMessage(`You gained ${profitPer}%. Your total gain is ${profit}`);
+
+            if(profitPer>50){
+                body.style.background="url(gifs/happy.gif)";
+            }
+
         }
+    }else{
+        showMessage(`Please enter values greater than 0 (Only numbers are valid in above fields)`)
     } 
 
 }
