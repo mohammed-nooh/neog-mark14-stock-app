0ce45fa085246e4f4f4d9113922c13ef9cd94366
mohammed-nooh <83730829+mohammed-nooh@users.noreply.github.com>
2021-07-31 12:43:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 "use strict";
 
 
-const inputs=document.querySelectorAll(".inputs");
+const inputs=document.querySelectorAll(".input");
 
 const subBtn=document.querySelector("#subBtn");
 const message=document.querySelector(".message");
 
+function showMessage(text){
+    message.classList.remove("hidden");
+    message.innerText=text;
+}
+
+
 function check(){
-    let [pur,no,sell]=[...inputs.values];
-    console.log(pur,no,sell);
+    message.classList.add("hidden");
+
+    const cp=Number(inputs[0].value);
+    const quantity=Number(inputs[1].value);
+    const sp=Number(inputs[2].value);
+    console.log(cp,quantity,sp);
+    console.log(cp-sp);
+    if(cp>0 && quantity>0 && sp>0){
+        //loss
+        if(cp>sp){
+            const loss=((cp-sp)*quantity).toFixed(2);
+            const lossPer=((cp-sp)*100/cp).toFixed(2);
+            showMessage(`You lost ${lossPer}%. Your total loss is ${loss}`);
+            console.log(`You lost ${lossPer}%. Your total loss is  ${loss}`);
+
+        }
+        //profit
+        else{
+            
+        }
+    } 
 
 }
 
